162f3058abe971f6560341f3d711bfefd27365d3
kuchiki111 <394957249@qq.com>
2017-06-06 20:04:02 +0800

    2016.6.7 20:03 USB Camera 画面预览

---
+++ Camera/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.Capture;
+using Windows.ApplicationModel;
+using System.Threading.Tasks;
+using Windows.System.Display;
+using Windows.Graphics.Display;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace Camera
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private WebCamHelper camera;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            initAsync();
+        }
+
+        public async void initAsync()
+        {
+            if (camera == null)
+            {
+                camera = new WebCamHelper();
+                await camera.InitializeCameraAsync();
+                cameraElement.Source = camera.mediaCapture;
+                photoAsync();
+            }
+
+        }
+
+        public  void photoAsync()
+        {
+            if (camera.IsInitialized())
+            {
+                tbMessage.Text = "success";
+            //    await TakePhotoAsync();
+                camera.StartCameraPreview();
+            }
+        }
+
+        public async Task TakePhotoAsync()
+        {
+            if (!camera.IsInitialized()) return;
+            StorageFile imgFile = await camera.CapturePhoto();
+        }
+
+        private void cameraElement_Loaded(object sender, RoutedEventArgs e)
+        {
+            initAsync();
+            
+        }
+    }
+}
---
+++ Camera/WebCamHelper.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+
+namespace Camera
+{
+    public class WebCamHelper
+    {
+        public MediaCapture mediaCapture;
+
+        private bool initialized = false;
+
+        /// <summary>
+        /// 异步初始化网络摄像头
+        /// </summary>
+        public async Task InitializeCameraAsync()
+        {
+            if (mediaCapture == null)
+            {
+                // 尝试发现摄像头
+                var cameraDevice = await FindCameraDevice();
+
+                if (cameraDevice == null)
+                {
+                    // 没有发现摄像头
+                    //Debug.WriteLine("No camera found!");
+                    initialized = false;
+                    return;
+                }
+
+                // Creates MediaCapture initialization settings with foudnd webcam device
+                var settings = new MediaCaptureInitializationSettings { VideoDeviceId = cameraDevice.Id };
+
+                mediaCapture = new MediaCapture();
+                await mediaCapture.InitializeAsync(settings);
+                initialized = true;
+            }
+        }
+
+        /// <summary>
+        /// 异步寻找摄像头，如果没有找到，返回null，否则返回DeviceInfomation
+        /// </summary>
+        private static async Task<DeviceInformation> FindCameraDevice()
+        {
+            // Get available devices for capturing pictures
+            var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+
+            if (allVideoDevices.Count > 0)
+            {
+                // 如果发现，返回
+                return allVideoDevices[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 开启摄像头预览
+        /// </summary>
+        public async Task StartCameraPreview()
+        {
+            try
+            {
+                await mediaCapture.StartPreviewAsync();
+            }
+            catch
+            {
+                initialized = false;
+                //Debug.WriteLine("Failed to start camera preview stream");
+            }
+        }
+
+        /// <summary>
+        /// 关闭摄像头预览
+        /// </summary>
+        public async Task StopCameraPreview()
+        {
+            try
+            {
+                await mediaCapture.StopPreviewAsync();
+            }
+            catch
+            {
+                //Debug.WriteLine("Failed to stop camera preview stream");
+            }
+        }
+
+
+        /// <summary>
+        /// 拍摄照片，返回StorageFile，文件将被存储到临时文件夹
+        /// </summary>
+        public async Task<StorageFile> CapturePhoto()
+        {
+            // Create storage file in local app storage
+            string fileName = GenerateNewFileName() + ".jpg";
+            CreationCollisionOption collisionOption = CreationCollisionOption.GenerateUniqueName;
+            StorageFile file = await ApplicationData.Current.TemporaryFolder.CreateFileAsync(fileName, collisionOption);
+
+            // 拍摄并且存储
+            await mediaCapture.CapturePhotoToStorageFileAsync(ImageEncodingProperties.CreateJpeg(), file);
+
+            //await Task.Delay(500);
+
+            return file;
+        }
+
+        /// <summary>
+        /// 产生文件名称
+        /// </summary>
+        private string GenerateNewFileName()
+        {
+            return " IoTSample" + DateTime.Now.ToString("yyyy.MMM.dd HH-mm-ss");
+        }
+
+        public string GenerateUserNameFileName(string userName)
+        {
+            return userName + DateTime.Now.ToString("yyyy.MM.dd HH-mm-ss") + ".jpg";
+        }
+
+        /// <summary>
+        /// 如果摄像头初始化成功，返回true，否则返回false
+        /// </summary>
+        public bool IsInitialized()
+        {
+            return initialized;
+        }
+    }
+}
